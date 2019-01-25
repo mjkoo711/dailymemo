3156785d515503a73806821cdf156be6b3aad6db
koominjun <mjkoo711@gmail.com>
2019-01-25 15:17:53 +0900

    init

---
+++ memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by MinJun KOO on 25/01/2019.
+//  Copyright © 2019 mjkoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  
+}
+
