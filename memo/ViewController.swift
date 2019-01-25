e58ff784781e55fc025918ab38c6f2238df9c1bd
koominjun <mjkoo711@gmail.com>
2019-01-26 05:37:59 +0900

    today 버튼 및 날짜표시완료

--- memo/ViewController.swift
+++ memo/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import FSCalendar
 
 class ViewController: UIViewController {
-  
+  @IBOutlet var calendarView: FSCalendar!
+  @IBOutlet var monthLabel: UILabel!
+
+  fileprivate let gregorian = Calendar(identifier: .gregorian)
+  fileprivate let formatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "YYYY-MM-dd"
+    return formatter
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    calendarView.placeholderType = .none
+    monthLabel.text = formatter.string(from: Date())
   }
-  
-  
+
+  @IBAction func todayTapped(_ sender: Any) {
+    print("HI")
+    calendarView.setCurrentPage(Date(), animated: false)
+    calendarView.select(Date())
+    monthLabel.text = formatter.string(from: Date())
+  }
+}
+
+extension ViewController: FSCalendarDelegate {
+  func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    monthLabel.text = formatter.string(from: date)
+
+  }
 }
 
