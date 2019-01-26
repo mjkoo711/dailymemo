28983d8737cb5d09a3e4d870eec13f2250baf6ab
koominjun <mjkoo711@gmail.com>
2019-01-26 14:00:14 +0900

    day of a week 표시 완료

--- memo/ViewController.swift
+++ memo/ViewController.swift
@@ -12,7 +12,8 @@ import FSCalendar
 class ViewController: UIViewController {
   @IBOutlet var calendarView: FSCalendar!
   @IBOutlet var monthLabel: UILabel!
-
+  @IBOutlet var dayLabel: UILabel!
+  
   fileprivate let gregorian = Calendar(identifier: .gregorian)
   fileprivate let formatter: DateFormatter = {
     let formatter = DateFormatter()
@@ -24,6 +25,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     calendarView.placeholderType = .none
     monthLabel.text = formatter.string(from: Date())
+    dayLabel.text = getStringDayOfWeek(weekDay: getDayOfWeek(formatter.string(from: Date())))
   }
 
   @IBAction func todayTapped(_ sender: Any) {
@@ -37,7 +39,67 @@ class ViewController: UIViewController {
 extension ViewController: FSCalendarDelegate {
   func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
     monthLabel.text = formatter.string(from: date)
-
+    dayLabel.text = getStringDayOfWeek(weekDay: getDayOfWeek(formatter.string(from: date)))
   }
 }
 
+extension ViewController {
+  private func getStringYear(year: String) -> String {
+    switch year {
+    case "01":
+      return "January"
+    case "02":
+      return "Fabruary"
+    case "03":
+      return "March"
+    case "04":
+      return "April"
+    case "05":
+      return "May"
+    case "06":
+      return "June"
+    case "07":
+      return "July"
+    case "08":
+      return "August"
+    case "09":
+      return "September"
+    case "10":
+      return "October"
+    case "11":
+      return "November"
+    case "12":
+      return "December"
+    default:
+      return ""
+    }
+  }
+
+  private func getStringDayOfWeek(weekDay: Int) -> String {
+    switch weekDay {
+    case 1:
+      return "Sunday"
+    case 2:
+      return "Monday"
+    case 3:
+      return "Tuesday"
+    case 4:
+      return "Wednesday"
+    case 5:
+      return "Thursday"
+    case 6:
+      return "Friday"
+    case 7:
+      return "Saturday"
+    default:
+      return ""
+    }
+  }
+
+  func getDayOfWeek(_ today:String) -> Int { // yyyy-MM-dd
+    let todayDate = formatter.date(from: today)
+    let myCalendar = Calendar(identifier: .gregorian)
+    let weekDay = myCalendar.component(.weekday, from: todayDate!)
+    return weekDay
+  }
+}
