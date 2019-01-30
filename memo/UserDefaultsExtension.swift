82df25a114e02412d5387f7e7b4ca07c1f8f9ef5
koominjun <mjkoo711@gmail.com>
2019-01-30 16:09:00 +0900

    UserDefaults에 텍스트 저장

---
+++ memo/UserDefaultsExtension.swift
@@ -0,0 +1,25 @@
+//
+//  UserDefaultsExtension.swift
+//  memo
+//
+//  Created by MinJun KOO on 30/01/2019.
+//  Copyright © 2019 mjkoo. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+  func decode<T : Codable>(for type : T.Type, using key : String) -> T? {
+    let defaults = UserDefaults.standard
+    guard let data = defaults.object(forKey: key) as? Data else {return nil}
+    let decodedObject = try? PropertyListDecoder().decode(type, from: data)
+    return decodedObject
+  }
+
+  func encode<T : Codable>(for type : T, using key : String) {
+    let defaults = UserDefaults.standard
+    let encodedData = try? PropertyListEncoder().encode(type)
+    defaults.set(encodedData, forKey: key)
+    defaults.synchronize()
+  }
+}
