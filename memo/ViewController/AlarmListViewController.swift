4f59c0f0d9b903e674386e1b4ea03c1603ff5abe
koominjun <mjkoo711@gmail.com>
2019-03-15 01:07:53 +0900

    alarm list에서 삭제 버튼 따로 만들기

--- memo/ViewController/AlarmListViewController.swift
+++ memo/ViewController/AlarmListViewController.swift
@@ -56,9 +56,14 @@ extension AlarmListViewController: UICollectionViewDelegate, UICollectionViewDat
     cell.textInstance = alarmTextDictionary[indexPath.section].value[indexPath.row]
 
     cell.textLabel.text = alarmTextDictionary[indexPath.section].value[indexPath.row].string
-    cell.dateLabel.text = alarmTextDictionary[indexPath.section].value[indexPath.row].date
+    cell.dateLabel.text = "작성 일자: " + alarmTextDictionary[indexPath.section].value[indexPath.row].date
 
     cell.delegate = self
+
+    // delete height size 임시방편 코드
+    let frame = CGRect(x: cell.deleteButtonView.frame.origin.x, y: cell.deleteButtonView.frame.minY, width: 40, height: cell.frame.height)
+    cell.deleteButtonView.frame = frame
+
     return cell
   }
 
@@ -76,14 +81,22 @@ extension AlarmListViewController: UICollectionViewDelegate, UICollectionViewDat
 
 extension AlarmListViewController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let constDateTextHeight = "text".height(withConstrainedWidth: UIScreen.main.bounds.width - 40, font: UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium))
+    let constDateTextHeight = "text".height(withConstrainedWidth: UIScreen.main.bounds.width - 40 - 40, font: UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium))
 
-    let height = alarmTextDictionary[indexPath.section].value[indexPath.row].string.height(withConstrainedWidth: UIScreen.main.bounds.width - 40, font: UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.medium)) + constDateTextHeight + 10
+    let height = alarmTextDictionary[indexPath.section].value[indexPath.row].string.height(withConstrainedWidth: UIScreen.main.bounds.width - 40 - 40, font: UIFont.systemFont(ofSize: 16, weight: UIFont.Weight.semibold)) + constDateTextHeight + 10
+
     return CGSize(width: UIScreen.main.bounds.width - 20, height: height)
   }
 }
 
 extension AlarmListViewController: AlarmCollectionViewCellDelegate {
+  func deleteAlarmText(text: Text) {
+    text.offAlarmSetting()
+    FMDBManager.shared.updateText(text: text)
+    AlarmManager().removeNotification(textSelected: text)
+    self.reloadCollectionView()
+  }
+
   func showActionSheet(text: Text) {
     textSelected = text
 
@@ -98,19 +111,9 @@ extension AlarmListViewController: AlarmCollectionViewCellDelegate {
       self.showAlarmSettingView()
     })
 
-    let deleteAlarm = UIAlertAction(title: "Remove Alarm", style: .destructive, handler: { (action) in
-      if let text = self.textSelected {
-        text.offAlarmSetting()
-        FMDBManager.shared.updateText(text: text)
-        AlarmManager().removeNotification(textSelected: text)
-        self.reloadCollectionView()
-      }
-    })
-
     let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
 
     actionSheet.addAction(setAlarmAction)
-    actionSheet.addAction(deleteAlarm)
     actionSheet.addAction(cancelAction)
 
     present(actionSheet, animated: true, completion: nil)
