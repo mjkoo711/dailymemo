d54ab2adcd53a803eba4e50268e1bf36fd02d1ee
koominjun <mjkoo711@gmail.com>
2019-03-15 23:30:30 +0900

    setting view 띄우는 방법 변경

--- memo/ViewController/SettingViewController.swift
+++ memo/ViewController/SettingViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+  @IBOutlet var closeButtonView: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    let tapGesture = UITapGestureRecognizer(target: self, action: #selector(returnMainViewController))
+    closeButtonView.addGestureRecognizer(tapGesture)
+  }
+}
+
+extension SettingViewController {
+  @objc func returnMainViewController() {
+    performSegue(withIdentifier: "unwindMainVC", sender: self)
   }
 }
