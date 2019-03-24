53990ac4a2ce8dbc5882ceed23aabc2e0624586f
koominjun <mjkoo711@gmail.com>
2019-03-24 17:41:52 +0900

    SettingVC 번역 완료

--- memo/ViewController/SettingViewController.swift
+++ memo/ViewController/SettingViewController.swift
@@ -14,13 +14,13 @@ protocol SettingViewControllerDelegate {
 }
 
 class SettingViewController: UIViewController {
-  let designList = ["테마", "메모 글자 크기", "메모 글자 두께", "진동", "단어잘림 방지", "알림권한 확인", "잠금 설정"]
-  let serviceList = ["프로버전 구매", "백업 / 복원", "리뷰 남기기", "문의메일 보내기"]
-  let size = ["작게", "중간", "크게"]
-  let thickness = ["얇게", "보통", "굵게"]
-  let onoff = ["끄기", "켜기"]
-  let theme = ["화이트 & 블루", "화이트 & 레드", "블랙 & 블루", "블랙 & 레드"]
-  let lock = ["업데이트 예정"]
+  let designList = ["Theme".localized, "Text Size".localized, "Text Thickness".localized, "Vibration".localized, "Prevent Word Truncation".localized, "Notification Permissions Check".localized, "Locking".localized]
+  let serviceList = ["Buy Pro Edition".localized, "Backup / Restore".localized, "Leave a Review".localized, "Contact Us".localized]
+  let size = ["Small".localized, "Middle".localized, "Big".localized]
+  let thickness = ["Thin".localized, "Regular".localized, "Bold".localized]
+  let onoff = ["Off".localized, "On".localized]
+  let theme = ["White & Blue".localized, "White & Red".localized, "Black & Blue".localized, "Black & Red".localized]
+  let lock = ["To Be Updated".localized]
 
   var delegate: SettingViewControllerDelegate?
   var date: String?
